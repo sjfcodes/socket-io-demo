407ebc529392e6565abeca07d216795555011a18
Samuel Fox <samueljasonfox@gmail.com>
2022-02-12 03:47:23 -0800

    update PORT, add deployment to readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # socket-io-demo
+
+A socket.io demo to explore the technology.
+
+- localhost connection, set a unique username and join the chat.
+
+[check it out](https://sjf-socketio.herokuapp.com/)
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
