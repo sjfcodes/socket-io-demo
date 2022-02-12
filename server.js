d849893c39d2a7c61df87ca7a966998eada0bf0a
Samuel Fox <samueljasonfox@gmail.com>
2022-02-11 19:41:16 -0800

    setup connection and public folder

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const path = require('path');
-const PORT = 3001;
-
+const path = require("path");
+const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("listening on *:3000");
 });
